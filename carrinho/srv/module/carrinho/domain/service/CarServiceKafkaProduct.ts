2af340fb66baeb7e4491cbf2cc26e681cd18ee78
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-06-27 10:07:51 -0300

    config kafkuser e kafkaproduct

--- carrinho/srv/module/carrinho/domain/service/CarServiceKafkaProduct.ts
+++ carrinho/srv/module/carrinho/domain/service/CarServiceKafkaProduct.ts
@@ -1,4 +1,24 @@
-export class CarServiceKafkaProduct{
+import { KafkaClient } from "../../infra/kafka/KafkaClient";
 
-    
+
+
+export async function startConsumerProduct() {
+
+    try {
+
+        const kafka = KafkaClient.getInstace()
+
+        const consumer = kafka.consumer({ groupId: 'cap-group' });
+        await consumer.connect();
+        await consumer.subscribe({ topic: 'product.updated', fromBeginning: false });
+
+        await consumer.run({
+            eachMessage: async ({ message }) => {
+                
+            }
+        })
+    } catch (error) {
+        console.error('Kafka indisponível, tentando novamente em 10s...', error);
+        setTimeout(startConsumerProduct, 10000);
+    };
 }
