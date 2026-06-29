ddf208006b067b67bafdabd9c491305777b25a2e
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-06-29 19:39:32 -0300

    add fotoProduto

--- produtos/demo/demo/src/main/java/com/example/demo/service/service/ProdutoRepositoryQueries.java
+++ produtos/demo/demo/src/main/java/com/example/demo/service/service/ProdutoRepositoryQueries.java
@@ -1,8 +1,9 @@
 package com.example.demo.service.service;
 
+import com.example.demo.service.entity.FotoProdutoEntity;
 import com.example.demo.service.model.FotoProduto;
 
 public interface ProdutoRepositoryQueries {
-    FotoProduto save(FotoProduto fotoProduto);
+    FotoProdutoEntity save(FotoProdutoEntity fotoProduto);
     void delete(FotoProduto foto);
 }
