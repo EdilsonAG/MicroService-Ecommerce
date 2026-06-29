91f894016aee99357b6977657373d543cd2bc680
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-06-29 19:46:01 -0300

    fix bug in product service add foto

--- produtos/demo/demo/src/main/java/com/example/demo/service/jpa/ProdutoRepositoryJPA.java
+++ produtos/demo/demo/src/main/java/com/example/demo/service/jpa/ProdutoRepositoryJPA.java
@@ -1,10 +1,12 @@
 package com.example.demo.service.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.service.entity.ProdutoEntity;
 import com.example.demo.service.service.ProdutoRepositoryQueries;
 
+@Repository
 public interface ProdutoRepositoryJPA extends JpaRepository<ProdutoEntity,Long> , ProdutoRepositoryQueries{
     
 }
