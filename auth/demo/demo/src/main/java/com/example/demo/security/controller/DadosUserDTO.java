2586014ef9c5b64c0e1ddf6f43c3223626930425
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-03 15:01:49 -0300

    trigger deploy

--- auth/demo/demo/src/main/java/com/example/demo/security/controller/DadosUserDTO.java
+++ auth/demo/demo/src/main/java/com/example/demo/security/controller/DadosUserDTO.java
@@ -5,8 +5,6 @@ public class DadosUserDTO {
     public record LoginRequest(String email, String password) {}
 
     public record RegisterRequest(String email, String pass, String name){}
-    public record RegisterRequest2(String email, String pass, String name){}
-    public record RegisterRequest3(String email, String pass, String name){}
-    public record RegisterRequest4(String email, String pass, String name){}
+    
 
 }
