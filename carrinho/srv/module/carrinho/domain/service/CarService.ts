74a6ae319454105fe7f4eb10038646deb2a6952b
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-04 12:14:44 -0300

    fix carrinho additemcarrinho

--- carrinho/srv/module/carrinho/domain/service/CarService.ts
+++ carrinho/srv/module/carrinho/domain/service/CarService.ts
@@ -4,6 +4,7 @@ import { CarRepository } from "../../interface/repository/CarRepository";
 import { CarRepositoryRedis } from "../../repository/CarRepositoryRedis";
 import { Carrinho } from "../model/Carrinho";
 import { ClienteEntity } from "../model/ClienteEntity";
+import { ItemCarrinho } from "../model/ItemCarrinho";
 
 export class CarService{
 
@@ -21,9 +22,10 @@ export class CarService{
         
         
          const carrinhoEncontrado = await this.carRepository.findCarByUserId(idUser)
+         const produtoEncontrado = await this.carRepository.findItemById(idProduto)
        
         
-        if(!carrinhoEncontrado){
+        if(!carrinhoEncontrado ){
             const user = new ClienteEntity();
             const carrinhoNovo = new Carrinho()
             user.id =  idUser 
@@ -31,8 +33,17 @@ export class CarService{
             this.carRepository.createCarrinho(carrinhoNovo)
         }
 
+        if(carrinhoEncontrado === null || produtoEncontrado === null){
+            throw new Error("sff")
+        }
+
+        const itemCarrinho = new ItemCarrinho()
+        itemCarrinho.carrinho = carrinhoEncontrado
+        itemCarrinho.produto = produtoEncontrado
+        itemCarrinho.quantidade = 5
+
         
-        //this.carRepository.addItemCarrinho(idUser)
+        this.carRepository.addItemCarrinho(idUser,itemCarrinho)
         
     }
 }
