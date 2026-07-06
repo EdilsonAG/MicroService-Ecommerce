ed2bf0a9557585f6bebd6f6b25e3ee263ad12c58
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-06 19:41:34 -0300

    liberando cors para desenvolvimento local

--- auth/demo/demo/src/main/java/com/example/demo/security/WebSecurity.java
+++ auth/demo/demo/src/main/java/com/example/demo/security/WebSecurity.java
@@ -1,8 +1,11 @@
 package com.example.demo.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +28,11 @@ public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws 
     System.out.println(("\n\n\n"));
     System.out.println(http);
     http
+    .cors(Customizer.withDefaults())
     .csrf(csrf -> csrf.disable())
     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
         .authorizeHttpRequests( authorize -> authorize
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                .requestMatchers("/auth/validate").permitAll() // nginx chama internamente
                .requestMatchers("/auth/login").permitAll()  
                .requestMatchers("/auth/register").permitAll()  
@@ -34,6 +42,19 @@ public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws 
   
         
     return http.build();
+}
+
+@Bean
+public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("http://localhost:5173"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
 }
 
  
