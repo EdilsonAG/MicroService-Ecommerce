9c9614fe20848b23a8be5fd23f82e41e976a7d85
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-06 11:57:59 -0300

    trigger deploy carrinho 2.0

--- carrinho/dist/srv/module/carrinho/domain/service/CarService.js
+++ carrinho/dist/srv/module/carrinho/domain/service/CarService.js
@@ -1,12 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CarService = void 0;
-const redisClient_1 = require("../../infra/redis/redisClient");
+const CarRepositoryRedis_1 = require("../../repository/CarRepositoryRedis");
+const Carrinho_1 = require("../model/Carrinho");
+const ClienteEntity_1 = require("../model/ClienteEntity");
+const ItemCarrinho_1 = require("../model/ItemCarrinho");
 class CarService {
+    constructor() {
+        this.carRepository = new CarRepositoryRedis_1.CarRepositoryRedis();
+    }
     criarCIarrinho() {
     }
     async addItemCarrinho(idUser, idProduto) {
-        const instancia = redisClient_1.RedisClient.getInstance();
+        console.log(idUser);
+        const carrinhoEncontrado = await this.carRepository.findCarByUserId(idUser);
+        const produtoEncontrado = await this.carRepository.findItemById(idProduto);
+        if (!carrinhoEncontrado) {
+            const user = new ClienteEntity_1.ClienteEntity();
+            const carrinhoNovo = new Carrinho_1.Carrinho();
+            user.id = idUser;
+            carrinhoNovo.user = user;
+            console.log("carrino não encontrado");
+            this.carRepository.createCarrinho(carrinhoNovo);
+        }
+        if (carrinhoEncontrado === null || produtoEncontrado === null) {
+            throw new Error("sff");
+        }
+        console.log("adicionar item no carrinho");
+        const itemCarrinho = new ItemCarrinho_1.ItemCarrinho();
+        itemCarrinho.carrinho = carrinhoEncontrado;
+        itemCarrinho.produto = produtoEncontrado;
+        itemCarrinho.quantidade = 5;
+        this.carRepository.addItemCarrinho(idUser, itemCarrinho);
     }
 }
 exports.CarService = CarService;
--- carrinho/dist/srv/module/carrinho/interface/repository/CarRepository.d.ts
+++ carrinho/dist/srv/module/carrinho/interface/repository/CarRepository.d.ts
@@ -1,8 +1,10 @@
 import { Carrinho } from "../../domain/model/Carrinho";
 import { ItemCarrinho } from "../../domain/model/ItemCarrinho";
+import { Product } from "../../domain/model/Product";
 export interface CarRepository {
     createCarrinho(car: Carrinho): Promise<void>;
     addItemCarrinho(userId: string, item: ItemCarrinho): Promise<void>;
     findCarByUserId(userId: string): Promise<Carrinho | null>;
+    findItemById(idItem: string): Promise<Product | null>;
 }
 //# sourceMappingURL=CarRepository.d.ts.map
