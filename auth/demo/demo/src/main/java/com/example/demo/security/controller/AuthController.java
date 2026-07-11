77519c728f21c137f4bc44f1c2efd6c9ac77e851
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-10 21:51:26 -0300

    trigger deploy

--- auth/demo/demo/src/main/java/com/example/demo/security/controller/AuthController.java
+++ auth/demo/demo/src/main/java/com/example/demo/security/controller/AuthController.java
@@ -265,6 +265,7 @@ System.out.println(origin);
         HttpSession session = request.getSession(false);
         // System.out.println(session.getId());
         if (session == null){
+            System.out.println("aqui não vai dar certo");
             return ResponseEntity.status(401).build();
         }
         System.out.println("De fato a uma sessao");
