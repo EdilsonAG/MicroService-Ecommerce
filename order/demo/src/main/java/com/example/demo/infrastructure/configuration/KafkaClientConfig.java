932b288accb6fb2f8e30c3f3aa9ebbc11ed34ab1
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-27 22:03:48 -0300

    add kafka consumer in order

--- order/demo/src/main/java/com/example/demo/infrastructure/configuration/KafkaClientConfig.java
+++ order/demo/src/main/java/com/example/demo/infrastructure/configuration/KafkaClientConfig.java
@@ -1,101 +1,101 @@
-package com.example.demo.infrastructure.configuration;
+// package com.example.demo.infrastructure.configuration;
 
  
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+// import org.apache.kafka.clients.consumer.ConsumerConfig;
+// import org.apache.kafka.clients.producer.ProducerConfig;
+// import org.apache.kafka.common.serialization.StringDeserializer;
+// import org.apache.kafka.common.serialization.StringSerializer;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+// import org.springframework.kafka.core.ConsumerFactory;
+// import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+// import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+// import org.springframework.kafka.core.KafkaTemplate;
+// import org.springframework.kafka.core.ProducerFactory;
+// import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+// import org.springframework.kafka.support.serializer.JsonDeserializer;
+// import org.springframework.kafka.support.serializer.JsonSerializer;
 
-@Configuration
-public class KafkaClientConfig {
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+// @Configuration
+// public class KafkaClientConfig {
+//     @Value("${spring.kafka.bootstrap-servers}")
+//     private String bootstrapServers;
 
-    @Value("${spring.kafka.security.protocol:PLAINTEXT}")
-    private String securityProtocol;
+//     @Value("${spring.kafka.security.protocol:PLAINTEXT}")
+//     private String securityProtocol;
 
-    @Value("${spring.kafka.properties.sasl.jaas.config:}")
-    private String jaasConfig;
+//     @Value("${spring.kafka.properties.sasl.jaas.config:}")
+//     private String jaasConfig;
 
-    @Value("${spring.kafka.ssl.trust-store-location:}")
-    private String trustStoreLocation;
+//     @Value("${spring.kafka.ssl.trust-store-location:}")
+//     private String trustStoreLocation;
 
-    @Value("${spring.kafka.ssl.trust-store-password:}")
-    private String trustStorePassword;
+//     @Value("${spring.kafka.ssl.trust-store-password:}")
+//     private String trustStorePassword;
 
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        config.put("security.protocol", securityProtocol);
+//     @Bean
+//     public ProducerFactory<String, Object> producerFactory() {
+//         Map<String, Object> config = new HashMap<>();
+//         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//         config.put("security.protocol", securityProtocol);
 
-        if ("SASL_SSL".equals(securityProtocol)) {
-            config.put("sasl.mechanism", "PLAIN");
-            config.put("sasl.jaas.config", jaasConfig);
-            config.put("ssl.truststore.location", trustStoreLocation.replace("file:", ""));
-            config.put("ssl.truststore.password", trustStorePassword);
-            config.put("ssl.endpoint.identification.algorithm", "");
-        }
+//         if ("SASL_SSL".equals(securityProtocol)) {
+//             config.put("sasl.mechanism", "PLAIN");
+//             config.put("sasl.jaas.config", jaasConfig);
+//             config.put("ssl.truststore.location", trustStoreLocation.replace("file:", ""));
+//             config.put("ssl.truststore.password", trustStorePassword);
+//             config.put("ssl.endpoint.identification.algorithm", "");
+//         }
 
-        return new DefaultKafkaProducerFactory<>(config);
-    }
+//         return new DefaultKafkaProducerFactory<>(config);
+//     }
 
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
+//     @Bean
+//     public KafkaTemplate<String, Object> kafkaTemplate() {
+//         return new KafkaTemplate<>(producerFactory());
+//     }
 
 
-    // consumer
+//     // consumer
 
-    @Value("${spring.kafka.consumer.group-id}")
-    private String groupId;
+//     @Value("${spring.kafka.consumer.group-id}")
+//     private String groupId;
 
-    @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.demo.*");
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        config.put("security.protocol", securityProtocol);
+//     @Bean
+//     public ConsumerFactory<String, Object> consumerFactory() {
+//         Map<String, Object> config = new HashMap<>();
+//         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//         config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+//         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+//         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+//         config.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+//         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+//         config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.demo.*");
+//         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//         config.put("security.protocol", securityProtocol);
 
-        if ("SASL_SSL".equals(securityProtocol)) {
-            config.put("sasl.mechanism", "PLAIN");
-            config.put("sasl.jaas.config", jaasConfig);
-            config.put("ssl.truststore.location", trustStoreLocation.replace("file:", ""));
-            config.put("ssl.truststore.password", trustStorePassword);
-            config.put("ssl.endpoint.identification.algorithm", "");
-        }
+//         if ("SASL_SSL".equals(securityProtocol)) {
+//             config.put("sasl.mechanism", "PLAIN");
+//             config.put("sasl.jaas.config", jaasConfig);
+//             config.put("ssl.truststore.location", trustStoreLocation.replace("file:", ""));
+//             config.put("ssl.truststore.password", trustStorePassword);
+//             config.put("ssl.endpoint.identification.algorithm", "");
+//         }
 
-        return new DefaultKafkaConsumerFactory<>(config);
-    }
+//         return new DefaultKafkaConsumerFactory<>(config);
+//     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-}
+//     @Bean
+//     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+//         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//         factory.setConsumerFactory(consumerFactory());
+//         return factory;
+//     }
+// }
